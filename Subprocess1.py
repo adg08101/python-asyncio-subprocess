8495e3412706d5257ab293341a1a3f173a880f2f
klol <adg08101@gmail.com>
2021-04-12 12:23:44 -0400

    Example
    Other example

---
+++ Subprocess1.py
@@ -0,0 +1,23 @@
+import asyncio
+import sys
+
+async def get_date():
+    code = 'import datetime; print(datetime.datetime.now())'
+
+    # Create the subprocess; redirect the standard output
+    # into a pipe.
+    proc = await asyncio.create_subprocess_exec(
+        sys.executable, '-c', code,
+        stdout=asyncio.subprocess.PIPE)
+
+    # Read one line of output.
+    data = await proc.stdout.readline()
+    line = data.decode('ascii').rstrip()
+
+    # Wait for the subprocess exit.
+    await proc.wait()
+    return line
+
+if __name__ == '__main__':
+    date = asyncio.run(get_date())
+    print(f"Current date: {date}")
